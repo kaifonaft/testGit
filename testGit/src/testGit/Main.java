9fcc85534a1cb485667a109c36de3b4605443cc9
alexander <kaifonaft@gmail.com>
2014-03-21 16:29:54 +0600

    first commit

---
+++ testGit/src/testGit/Main.java
@@ -0,0 +1,15 @@
+package testGit;
+
+import java.io.PrintWriter;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PrintWriter out = new PrintWriter(System.out);
+		out.print("hello world");
+		out.flush();
+	}
+
+}
